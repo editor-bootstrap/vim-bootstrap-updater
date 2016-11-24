87af0b222bc525a3b98dcd1b91dba9bf26dea81c
cassiobotaro <cassiobotaro@gmail.com>
2016-11-24 10:08:05 -0200

    Python 2/3 compability

--- plugin/vim_bootstrap_updater.py
+++ plugin/vim_bootstrap_updater.py
@@ -1,5 +1,4 @@
 import os
-import sys
 try:
     import urllib2
     import urllib
@@ -7,26 +6,27 @@ except ImportError:
     import urllib.request as urllib2
     import urllib.parse as urllib
 
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 HEADERS = {
     'User-Agent': 'Mozilla/5.0',
 }
 
 
 def _generate_vimrc(editor, langs):
+    '''Generate a config file.
+
+    Generate a config file based on your editor and choosen languages.
+    '''
     params = [('langs', l.strip()) for l in langs]
     params.append(('editor', editor))
     data = urllib.urlencode(params)
     URL = "https://vim-bootstrap.com/generate.vim"
     req = urllib2.Request(URL, headers=HEADERS)
     resp = urllib2.urlopen(req, data.encode('utf-8'))
-    return resp.read()
+    return resp.read().decode('utf-8')
 
 
 def get_available_langs():
+    '''Get vim-bootstrap available languages'''
     URL = "https://vim-bootstrap.com/langs"
     req = urllib2.Request(URL, headers=HEADERS)
     resp = urllib2.urlopen(req)
@@ -34,10 +34,14 @@ def get_available_langs():
 
 
 def update(vimrc, editor, langs):
-    content = _generate_vimrc(editor, langs).decode('utf-8')
+    '''Overwrite vim/nvim config file with new configs.
+
+    Overwrite vim/nvimrc config file based on your choosen languages.
+    '''
+    content = _generate_vimrc(editor, langs)
     vimrc = os.path.expanduser(vimrc)
 
-    with open(vimrc, 'w') as fh:
-        fh.write(content)
+    with open(vimrc, 'wb') as fh:
+        fh.write(content.encode('utf-8'))
 
     return content
